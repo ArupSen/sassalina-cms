0f95540763fbda364d6b89446db61ab43ae3779d
Arup Sen <arupsen48@gmail.com>
2015-10-16 00:20:33 +0100

    Add dolly template

---
+++ dolly_template.php
@@ -0,0 +1,27 @@
+<?php
+/* Template Name: Dolly Pages */
+?>
+<?php get_header(); ?>
+
+<div id="contents">
+  <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+    <div id="post-<?php the_ID(); ?>" <?php post_class('page'); ?>>
+      <div class="container">
+<h2>dolly</h2>
+<a href="<?php bloginfo('url'); ?>/dolly/" title="back to dolly" class="btn btn-default back-button"><i class="fa fa-angle-double-left"></i> dolly</a>
+<div class="details">
+        <h2><?php the_title(); ?></h2>
+<hr>
+        <div id="page-content">
+          <?php the_content(); ?>
+        </div><!--#pageContent -->
+</div><!-- .details -->
+      </div>
+
+    </div><!--#post-# .post-->
+
+
+  <?php endwhile; ?>
+</div><!--#content-->
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
